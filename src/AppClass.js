f39eaefea2631946869aa038005463fcb513e0f1
yuyaoowada <yuyaoowada@gmail.com>
2023-08-11 22:53:45 +0900

    Hello World

---
+++ src/AppClass.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+    render() {
+        return <h1>Hello World!</h1>;
+    }
+}
---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function HelloWorld() {
+    return (
+        <Fragment>
+            <hr />
+            <h1>Hello World!</h1>
+        </Fragment>
+    );
+}
+
+export default HelloWorld;
